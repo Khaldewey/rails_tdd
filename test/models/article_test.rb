a7571c0668f1d73d5ae68513f2e44971e2b189d6
Israel Alves <israel_alves77@hotmail.com>
2024-07-14 10:36:04 -0300

    dando uma treinada em fixtures

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -5,4 +5,10 @@ class ArticleTest < ActiveSupport::TestCase
     article = Article.new
     assert_not article.save
   end
+
+  test "should do some interesting with stored state" do
+   assert_equal "My first post", articles(:one).title
+   assert_equal "Second Post", articles(:two).title
+   assert_equal 1002, Article.count
+  end
 end
