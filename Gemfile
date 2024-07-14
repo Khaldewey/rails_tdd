4a564948726feaf758700ef42e9051fb684a0b78
Israel Alves <israel_alves77@hotmail.com>
2024-07-14 14:27:20 -0300

    teste de integração

--- Gemfile
+++ Gemfile
@@ -15,6 +15,10 @@ gem 'httparty'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.2.0'
 
+gem 'turbolinks', '~> 5' 
+
+gem 'wdm', '>= 0.1.0'
+
 # gem 'rails-i18n', '~> 5.1', '>= 5.1.3'
 
 # Use SCSS for stylesheets
---
+++ test/integration/blog_flow_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class BlogFlowTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "can view articles list from root route" do
+   get '/'
+   assert_select 'h1', 'Articles'
+  end
+
+  test "can create an article" do 
+   get '/articles/new'
+   assert_response :success 
+   post '/articles', params: { article: { title: "My post title"}, content: "My post content"}
+   assert_response :redirect
+   follow_redirect!
+   assert_response :success
+   assert_select 'p', 'Article was successfully created.'  
+  end
+end
