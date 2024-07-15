baa9879b236a13a4aeec53bed1149f7511524d82
israel <Israel@AGENCIAACERTE.LOCAL>
2024-07-15 10:13:00 -0300

    teste com dependência temporal

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
  validates :title, presence: true
+
+ def archived?
+    Date.current > self.created_at + 30.days
+ end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -11,4 +11,15 @@ class ArticleTest < ActiveSupport::TestCase
    assert_equal "Second Post", articles(:two).title
    assert Article.count >= 1000
   end
+
+  test "should be archived after 30 days" do
+    first = Article.first
+    assert_not first.archived?
+    puts "Data Atual: #{Date.current}"
+    future = Date.current + 31.days
+    travel_to future do
+      puts "Data Futura: #{future}"
+      assert first.archived?
+    end
+  end
 end
